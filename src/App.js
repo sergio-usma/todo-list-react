73f510e2c5f2414a9385fed8e2df7cfd6ae6be5f
sergio-usma <sergioandresusma@hotmail.com>
2023-07-10 16:59:10 -0500

    Render CreateArea component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Header from './components/Header.jsx';
+import CreateArea from './components/CreateArea.jsx';
+import React, { useState } from "react";
 
 function App() {
+
+const [notes, setNotes] = useState([]);
+
+function addNote (newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+}
+
   return (
     <div className="App">
       <Header />
+        <CreateArea onAdd={addNote}/>
     </div>
   );
 }
