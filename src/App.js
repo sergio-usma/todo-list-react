ed44bbab10e49eae8aa6b28c8a85e7078ca7b2ca
sergio-usma <sergioandresusma@hotmail.com>
2023-07-10 17:43:40 -0500

    Render NotesWrapper component to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header.jsx';
 import CreateArea from './components/CreateArea.jsx';
 import React, { useState } from "react";
+import Note from './components/NotesWrapper.jsx';
 
 function App() {
 
@@ -13,10 +14,29 @@ function addNote (newNote) {
     });
 }
 
-  return (
+function deleteNote (id) {
+    setNotes(prevNotes => {
+        return prevNotes.filter((noteItem, index) => {
+            return index !== id;
+        });
+    });
+}
+
+    return (
     <div className="App">
       <Header />
         <CreateArea onAdd={addNote}/>
+        {notes.map((noteItem, index) => {
+            return (
+                <Note
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            )}
+        )}
     </div>
   );
 }
